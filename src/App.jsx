f910d16ee3b50e4ad6aafe5c2f40b8413c65b715
Sisi Wang <sisiwang242@gmail.com>
2024-11-25 17:12:49 -0500

    week4 - Lifting state and props handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  }
 
   return (
     <div className="w-full flex flex-col justify-center items-center p-10 gap-4">
       <h2 className='text-2xl font-serif font-bold'>Todo List</h2>
-      <AddTodoForm onAddTodo={setNewTodo}/>
-      <p>{newTodo}</p>
-      <TodoList />
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   )
-};
+}
 
 export default App
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,22 @@
-import lists from './Data.jsx';
-import TodoListItem from './TodoListItem.jsx';
+const TodoList = ({ todoList }) => {
+  return (
+    <ul className="flex flex-col gap-2 text-lg">
+      {todoList.length > 0 ? (
+        todoList.map((item, index) => (
+          <li key={item.id || index} className="hover:shadow-xl shadow-gray-400 p-2">
+            <span>{index + 1}.</span>
+            <a href={item.url} alt={item.title} className="font-bold">
+              {item.title}
+            </a>
+            <span> -- due {item.due}</span>
+          </li>
+        ))
+      ) : (
+        <li className="text-gray-500">No Todo added yet!</li>
+      )}
+    </ul>
+  );
+};
 
-const TodoList = () => {
-    return (
-      <ul className='flex flex-col gap-2 text-lg'>
-        {
-          lists.map((todo) => (
-            <TodoListItem key={todo.id} todo={todo} />
-          ))
-        }
-      </ul>
-    );
-  }
+export default TodoList;
 
-  export default TodoList; 
